9d5dbfe6a46e06675ac457967c8ebefcad6d52e8
root <root@ip-172-31-34-86.ec2.internal>
2016-03-18 04:32:02 +0000

    third commit

--- c.py
+++ c.py
@@ -1,13 +1,12 @@
 import os
 import sys
 from pyspark import SparkConf, SparkContext
+from pyspark.sql import SQLContext
 APP_NAME = "My Spark Application"
-def main(sc):
-  rdd = sc.parallelize(range(1000), 10)
-  print rdd.mean()
-if __name__ == "__main__":
-# Configure OPTIONS
+if __name__=="__main__":
   conf = SparkConf().setAppName(APP_NAME)
   conf = conf.setMaster("local[*]")
-#in cluster this will be like
-#"spark://ec2-0-17-03-078.compute
+  sc = SparkContext(conf=conf)
+  sqlContext = SQLContext(sc)
+  df = sqlContext.read.json("tweets/*.json")
+  df.printSchema()

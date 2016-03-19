ffedd97a03da851dbe7dec782ad963e74b54ff47
root <root@ip-172-31-33-188.ec2.internal>
2016-03-19 21:35:36 +0000

    second

--- c.py
+++ c.py
@@ -8,5 +8,7 @@ if __name__=="__main__":
   conf = conf.setMaster("local[*]")
   sc = SparkContext(conf=conf)
   sqlContext = SQLContext(sc)
-  df = sqlContext.read.json("tweets/*.json")
-  
+  df = sqlContext.read.json("tweets2/cleaned_2016_01_19.json")
+  df1=sc.parallelize(df.take(10))
+  df1.saveAsTextFile("tweets3/hahahah.txt")
+  #print df1

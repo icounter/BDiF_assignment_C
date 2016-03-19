06ba8b254123f72bc0fa607fbe0a0dfbf78dc46a
root <root@ip-172-31-33-188.ec2.internal>
2016-03-19 19:53:08 +0000

    first

--- yahoo_quotes.py
+++ yahoo_quotes.py
@@ -1,35 +1,16 @@
-"""
-A basic script to help with Assignment C.
-This can be used to generate an array of closing values for selected stocks for a given date range, using the Yahoo API.
-"""
-
+import csv as csv
+import pandas as pd
 import numpy as np
-import datetime
-
 import matplotlib.pyplot as plt
-from matplotlib.finance import quotes_historical_yahoo
-
-symbol_dict = {
-    'GS': 'Goldman Sachs',
-}
-
-symbols, names = np.array(list(symbol_dict.items())).T
-
-startdate = datetime.datetime(2012, 1, 1)
-enddate = datetime.datetime(2014, 12, 31)
-
-quotes = []
-for symbol in symbols:
-	try:
-		qt = quotes_historical_yahoo(symbol, startdate, enddate, asobject=True)
-		quotes.append(qt)
-	except:
-		print("fail: ", symbol)
-
-
-close = np.array([q.close for q in quotes])
-print close
-
-
-
-
+import math
+import random as rd
+import pandas.io.data as web
+from datetime import datetime
+SYMBOL=['GS','GOOG']
+start = datetime(2012,12,31)
+end = datetime(2014,1,1)
+stockRawData = web.DataReader(SYMBOL, 'yahoo', start, end)
+sliceKey = 'Adj Close'
+adjCloseData = stockRawData.ix[sliceKey]
+adjCloseData=adjCloseData.shift(1)/adjCloseData-1
+adjCloseData.to_csv("stockreturn.txt")

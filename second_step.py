66976eeaac5a5cf86b3223b5a63617c149730a6b
chaozhu <peterchaoz93@gamil.com>
2016-03-20 13:01:23 -0400

    sss

--- second_step.py
+++ second_step.py
@@ -8,11 +8,13 @@ if __name__=="__main__":
   conf = conf.setMaster("local[*]")
   sc = SparkContext(conf=conf)
   df = sc.textFile("tweets/*.txt")
-  parts = df.map(lambda l: l.split(","))
+  parts = df.map(lambda x: (x.split(" ")[0], x)))
   #df1=df.take(1)
   #print df1
   df2=sc.textFile("tws4")
-  parts=df2.map(lambda l: l.split("\t"))
+  parts2=df2.map(lambda l: ((l.split("\t")[0],l)))
+  parts3=parts.join(parts2)
+
   #df3=df2.take(1)
   #print df3
 

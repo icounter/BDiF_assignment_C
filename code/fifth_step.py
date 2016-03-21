284da0e3c2289d696663488359e7483339a7b3b0
peterchaozhu <peterchaoz93@gmail.com>
2016-03-21 08:27:32 -0400

    Update fifth_step.py

--- code/fifth_step.py
+++ code/fifth_step.py
@@ -1,5 +1,6 @@
 __author__ = 'zhuchao1'
 #import file
+#linear regression file
 from pyspark.mllib.regression import LabeledPoint, LinearRegressionWithSGD, LinearRegressionModel
 import os
 import random
@@ -17,17 +18,19 @@ def parsePoint(ss):
   return LabeledPoint(values[0], values[1:])
 
 if __name__=="__main__":
+  ##set APP_NAME
   conf = SparkConf().setAppName(APP_NAME)
   conf = conf.setMaster("local[*]")
   sc = SparkContext(conf=conf)
-
-  data = sc.textFile("step3")
+  #Set datasource 
+  data = sc.textFile("step4")
   train_data = data.map(parsePoint)
-
-  # Build the model
+  
+  # Build the model iteratiobn=100 threshold=0.1
   model = LinearRegressionWithSGD.train(train_data,100,0.1)
 
   # Evaluate the model on training data
+  #to calculate MSE
   valuesAndPreds = train_data.map(lambda p: (p.label, model.predict(p.features)))
   train_res = valuesAndPreds.map(lambda (v, p): (v - p)**2).reduce(lambda x, y: x + y) / valuesAndPreds.count()
-  train_res.saveAsTextFile("step4")
+  train_res.saveAsTextFile("step5")
